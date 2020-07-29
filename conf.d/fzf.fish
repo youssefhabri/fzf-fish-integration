92eb27b98d6b06949256c762d0bfa321c2e7f3a7
Youssef Habri <youssef.habri@gmail.com>
2020-07-29 17:31:16 +0100

    Change git log binding to ctrl+g

--- conf.d/fzf.fish
+++ conf.d/fzf.fish
@@ -2,7 +2,7 @@
 if not set --query fzf_fish_custom_keybindings
     # \cf is ctrl+f, etc.
     bind \cf '__fzf_search_current_dir'
-    bind \cl '__fzf_search_git_log'
+    bind \cg '__fzf_search_git_log'
     bind \cr '__fzf_search_history'
     bind \cv '__fzf_search_shell_variables'
 end
